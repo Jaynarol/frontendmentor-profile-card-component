39006af03e6986a191294fb83f5195c52cac66bb
Jaynarol <jaynarol@gmail.com>
2023-08-25 19:57:49 +0700

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kumbh: ["Kumbh Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "hsl(229, 23%, 23%)",
+        secondary: "hsl(227, 10%, 46%)",
+        rear: "hsl(185, 75%, 39%)",
+      },
+      backgroundImage: {
+        "pattern-card": "url('./images/bg-pattern-card.svg')",
+      },
+    },
   },
   plugins: [],
 };
